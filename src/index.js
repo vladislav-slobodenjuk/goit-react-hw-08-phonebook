9ebf1cbd6e9b52748cf17b3b00f5cb668ebbe9b6
vladislav-slobodenjuk <vladjun@gmail.com>
2022-01-31 20:44:57 +0200

    more npm packeges, commented persistor, base css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'components/App/App.jsx';
-
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
-
-import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from 'components/App/App.jsx';
+import { store, persistor } from 'redux/store';
+import 'modern-normalize/modern-normalize.css';
+import 'styles/base.scss';
+// import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 import { contactsReducer } from './contacts/contacts-reducers';
+// import { authReducer } from './auth';
+
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
 
 export const store = configureStore({
   reducer: {
+    // auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
+
+export const persistor = persistStore(store);
